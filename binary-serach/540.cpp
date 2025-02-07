24ab47ebe8243901bf8a96ed1463c1aa51293885
sh3bh1t <shobhits2000@gmail.com>
2025-02-07 15:38:27 +0530

    -------

---
+++ binary-serach/540.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1){
+            return nums[0];
+        }
+        for(int i=0;i<n;i++){
+            if(nums[0]!=nums[1]){
+                return nums[0];
+            }
+            if(nums[n-1]!=nums[n-2]){
+                return nums[n-1];
+            }
+            if(nums[i]!=nums[i+1] && nums[i]!=nums[i-1]){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+};
